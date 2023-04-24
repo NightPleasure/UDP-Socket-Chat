b3e49b0e664d019d5a19b4610b3f45c9d28ba84c
NightPleasure <barbaian.victor@gmail.com>
2023-04-25 01:37:44 +0300

    added private message feature

--- UDP.py
+++ UDP.py
@@ -12,7 +12,6 @@ nickname = input("Enter your nickname: ")
 # Store a mapping of addresses to nicknames
 nickname_map = {}
 
-
 def receive_messages():
     while True:
         data, address = sock.recvfrom(1024)
@@ -27,14 +26,29 @@ def receive_messages():
         nickname = nickname_map.get(address, str(address))
         print("{}: {}".format(nickname, message.strip()))
 
+def send_private_message():
+    while True:
+        input_string = input('')
+        if input_string.startswith('/pm'):
+            inputs = input_string.split(' ')
+            recipient_nickname = inputs[1]
+            message = ' '.join(inputs[2:])
+            for address, nickname in nickname_map.items():
+                if nickname == recipient_nickname:
+                    message = "{} (private): {}".format(nickname, message)
+                    sock.sendto(message.encode(), (address[0], 1234))
+                    break
+        else:
+            message = "{}: {}".format(nickname, input_string)
+            sock.sendto(message.encode(), ('<broadcast>', 1234))
 
 receiver_thread = threading.Thread(target=receive_messages)
 receiver_thread.daemon = True
 receiver_thread.start()
 
-while True:
-    message = input("\nEnter your message: ")
+sender_thread = threading.Thread(target=send_private_message)
+sender_thread.daemon = True
+sender_thread.start()
 
-    # Send the message with the nickname as a prefix
-    message = "{}: {}".format(nickname, message)
-    sock.sendto(message.encode(), ('<broadcast>', 1234))
+while True:
+    pass
