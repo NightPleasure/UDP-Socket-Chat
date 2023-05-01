e1594def242bc3542a25e311c3da2271136c063d
NightPleasure <barbaian.victor@gmail.com>
2023-05-01 21:27:47 +0300

    modified private message feature

--- UDP.py
+++ UDP.py
@@ -4,55 +4,40 @@ import threading
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-sock.bind(('', 1234))
+sock.bind(('', 40404))
 
-# Store the user's nickname
 nickname = input("Enter your nickname: ")
 
-# Store a mapping of addresses to nicknames
 nickname_map = {}
 
 
 def receive_messages():
     while True:
-        data, address = sock.recvfrom(1024)
+        data, address = sock.recvfrom(40404)
         message = data.decode()
 
-        # Extract the nickname from the message (if present)
         if ':' in message:
             nickname, message = message.split(':', 1)
             nickname_map[address] = nickname.strip()
 
-        # Print the message with the sender's nickname (if available)
         nickname = nickname_map.get(address, str(address))
         print("{}: {}".format(nickname, message.strip()))
 
 
-receiver_thread = threading.Thread(target=receive_messages)
-receiver_thread.daemon = True
-receiver_thread.start()
+receiver_thread = threading.Thread(target=receive_messages).start()
 
 while True:
     message = input("\nEnter your message: ")
 
-    if message.startswith('@'):
-        # Private message
-        parts = message.split(' ', 1)
-        if len(parts) < 2:
-            print('Usage: @<nickname> <message>')
-            continue
-
-        nickname, message = parts
-        message = "{} (private): {}".format(nickname, message)
-        for address, name in nickname_map.items():
-            if name == nickname:
-                sock.sendto(message.encode(), address)
-                break
+    if message.startswith('/pm'):
+        parts = message.split(':', 2)
+        if len(parts) == 3:
+            ip = parts[1].strip()
+            message = "{}: {}".format(nickname, parts[2].strip())
+            sock.sendto(message.encode(), (ip, 40404))
+            print("Sent private message to {}".format(ip))
         else:
-            print('No user with nickname {} found'.format(nickname))
-            continue
-
+            print("Invalid private message format. Usage: /pm IP_ADDRESS: MESSAGE")
     else:
-        # Broadcast message
         message = "{}: {}".format(nickname, message)
-        sock.sendto(message.encode(), ('<broadcast>', 1234))
+        sock.sendto(message.encode(), ('<broadcast>', 40404))
